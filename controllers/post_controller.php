5da802ec56c85eecf8879e9131314d081c3e1b32
HodheilMLLL <hodheil.mlll@eduge.ch>
2022-03-10 13:17:41 +0100

    Améliorations
    Les fichiers ne sont plus sauvegardés si une erreur est rencontrée lors du post
    
    Les rollBack() ont été renplacés par des exeptions

--- controllers/post_controller.php
+++ controllers/post_controller.php
@@ -22,7 +22,8 @@ switch ($action) {
             $taille_maxi_tout = 70000000; // 70Mo en octets
             $extensions = array('.png', '.gif', '.jpg', '.jpeg', '.mp4', '.mov', '.wmv', '.avi', '.mkv', '.webm', '.m4a', '.flac', '.mp3', '.wav', '.wma', '.aac');
             $countfiles = count($_FILES['myImg']['name']);
-            
+            $createdFiles = array();
+
             unset($erreur);
 
             for ($i = 0; $i < $countfiles; $i++) {
@@ -35,13 +36,13 @@ switch ($action) {
                 if (!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
                 {
                     $erreur = 'Vous devez uploader un fichier de type png, gif, jpg ou jpeg';
-                    // Annulation de la transsction
-                    MonPdo::getInstance()->rollBack();
+                    // Annulation de la transaction
+                    throw new Exception;
                 }
                 if ($taille > $taille_maxi) {
                     $erreur = "Taille de fichier(s) dépassant la limite";
-                    // Annulation de la transsction
-                    MonPdo::getInstance()->rollBack();
+                    // Annulation de la transaction
+                    throw new Exception;
                 }
 
                 if (!isset($erreur)) // S'il n'y a pas d'erreur, on upload
@@ -50,6 +51,9 @@ switch ($action) {
                     $newfilename = round(microtime(true)) . $i . '.' . end($temp);
                     if (move_uploaded_file($_FILES['myImg']['tmp_name'][$i], "$dossier/" . $newfilename)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
                     {
+                        // Ajout du fichier dans le tableau des fichiers créés
+                        array_push($createdFiles, $newfilename);
+
                         // Insertion dans la base de données
                         $media = new Media();
                         $media->setTypeMedia($_FILES["myImg"]["type"][$i]);
@@ -57,21 +61,21 @@ switch ($action) {
                         Media::addMedia($media, $idPost);
                     } else //Sinon (la fonction renvoie FALSE).
                     {
-                        // Annulation de la transsction
-                        MonPdo::getInstance()->rollBack();
+                        // Annulation de la transaction
+                        throw new Exception;
                     }
                 }
+            }
+            // Vérification du non-dépassement de la limite de 70Mo
+            if ($taille_tout > $taille_maxi_tout) {
+                // Annulation de la transasction
+                $erreur = "Fichier(s) trop lourd(s)";
+                throw new Exception;
+                
             }
         } catch (\Throwable $th) {
             // Si une erreur est rencontrée, annulation de la transaction
             MonPdo::getInstance()->rollBack();
-        }
-
-        // Vérification du non-dépassement de la limite de 70Mo
-        if ($taille_tout > $taille_maxi_tout){
-            // Annulation de la transsction
-            MonPdo::getInstance()->rollBack();
-            $erreur = "Taille de fichier(s) dépassant la limite";
         }
 
         try {
@@ -81,6 +85,11 @@ switch ($action) {
             // Affichage d'un message d'erreur
             $_SESSION['messageAlert']['type'] = "danger";
             $_SESSION['messageAlert']['message'] = $erreur;
+
+            // Suppression des fichiers qui ont été crées
+            foreach ($createdFiles as $file) {
+                unlink("$dossier/" .$file);
+            }
         }
 
         if (!isset($erreur)) // S'il aucune erreur n'a été rencontrée
